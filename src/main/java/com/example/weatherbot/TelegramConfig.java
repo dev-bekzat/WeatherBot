1bd4e08a38fb0cddd1c6a87edd1468849ce110fd
static <dev.bekzat@gmail.com>
2024-02-26 13:27:10 +0600

    tiny update

--- src/main/java/com/example/weatherbot/TelegramConfig.java
+++ src/main/java/com/example/weatherbot/TelegramConfig.java
@@ -3,7 +3,6 @@ package com.example.weatherbot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
